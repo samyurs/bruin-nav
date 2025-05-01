62109b4f2864b0ceefeba6d51d403f10fc4a021b
Najm Hoda <najmkhoda@gmail.com>
2025-04-30 19:03:32 -0700

    Add Landmark mongoose model

---
+++ Backend/models/Landmark.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const LandmarkSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    type: { type: String, enum: [
+        'building', 'restroom', 'study-spot',
+        'classroom', 'printer'
+    ] },
+    location: GeoJSONSchema,
+    hours: {
+        type: [HoursSchema],
+        validate: { validator: hours => hours.length === 7 },
+    },
+    parent: { type: mongoose.Types.ObjectId, ref: 'Landmark' },
+});
+LandmarkSchema.index({ location: '2dsphere' });
+
+const HoursSchema = new mongoose.Schema({
+    isOpen: { type: Boolean, required: true },
+    open: {
+        type: Number,
+        min: 0,
+        max: function() { return this.close; },
+        required: function() { return this.isOpen; },
+    },
+    close: {
+        type: Number,
+        max: 1439,
+        required: function() { return this.isOpen; }
+    },
+}, { _id: false });
+
+const GeoJSONSchema = new mongoose.Schema({
+    type: { type: String, enum: ['Point'], required: true },
+    coordinates: {
+        type: [Number],
+        required: true,
+        validate: {
+            validator: function(coords) {
+                return coords.length === 2 &&
+                    coords[0] >= -180 && coords[0] <= 180 &&
+                    coords[1] >= -90 && coords[1] <= 90;
+            }
+        }
+    }
+}, { _id: false });
+
+module.exports = mongoose.models.Landmark || mongoose.model('Landmark', LandmarkSchema);
