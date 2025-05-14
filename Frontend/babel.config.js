1c709f0af522fdc807d00ad0efda6d6db5c34d5d
krishabasrur123 <krishabasrur@gmail.com>
2025-05-14 15:49:18 -0700

    Search (UI)

--- Frontend/babel.config.js
+++ Frontend/babel.config.js
@@ -1,10 +1,11 @@
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins:  [
-    'module:react-native-dotenv', 
-    {
+  plugins: [
+    ['module:react-native-dotenv', {
       moduleName: '@env',
       path: '.env',
-    },
+    }],
   ],
 };
+
