890c84709ad61693eaa5e06238626ec248d54622
pegifenu <pegifenu@gmail.com>
2025-05-29 19:48:23 -0700

    undo minor localhost change

--- Frontend/src/screens/SettingsScreen.js
+++ Frontend/src/screens/SettingsScreen.js
@@ -23,7 +23,7 @@ export default function SettingsScreen({ route }) {
     }
 
     try {
-      const res = await fetch(`${API_BASE_URL}/users`, {
+      const res = await fetch(`${API_BASE_URL}/users/update`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
